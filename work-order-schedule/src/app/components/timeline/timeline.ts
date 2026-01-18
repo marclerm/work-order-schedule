f6a2557d69511348059c854f765014bc0a051bc2
Marco Lerma <marcolerma.1312@gmail.com>
2026-01-18 14:21:31 -0500

    Implement current indicator and update graph scale based on timescale slector value (2hrs)
    - Generate timeline columns for Day, Week, and Month views
    - Adjust column widths based on selected timescale
    - Add current date indicator with correct positioning per scale
    - Auto scroll timeline to keep current period in view
    - Update some styles to match with colors from _tokens

--- work-order-schedule/src/app/components/timeline/timeline.ts
+++ work-order-schedule/src/app/components/timeline/timeline.ts
@@ -1,8 +1,9 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, computed, signal, AfterViewInit, ElementRef, ViewChild } from '@angular/core';
 import { TimescaleSelector } from '../timescale-selector/timescale-selector';
 import { Timescale } from '../../models/timeline.types';
 import { WORK_CENTERS } from '../../data/sample-data';
+import { buildColumnRanges, TimelineColumnRange } from '../../utils/timeline-range';
 
 @Component({
   selector: 'app-timeline',
@@ -11,8 +12,99 @@ import { WORK_CENTERS } from '../../data/sample-data';
   templateUrl: './timeline.html',
   styleUrls: ['./timeline.scss'],
 })
-export class Timeline {
-  timeScale: Timescale = 'month';
+export class Timeline implements AfterViewInit {
+  // Props
+  timeScale = signal<Timescale>('month');
   workCenters = WORK_CENTERS;
   monthColumns = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+  today = new Date();
+
+  // Elements
+  @ViewChild('scrollElement', { static: true }) scrollElement!: ElementRef<HTMLDivElement>;
+
+  //#region  Start columns build and width for Header and Rows
+  columns = computed<TimelineColumnRange[]>(() => buildColumnRanges(this.timeScale()));
+  columnWidthPx = computed<number>(() => {
+    const t = this.timeScale();
+    if(t === 'month') return 160;
+    if(t === 'week') return 140;
+    return 60; // day
+  });
+  //#endregion
+
+  //#region Timescale Selector handling
+
+  onTimeScaleChange(value: Timescale) {
+    this.timeScale.set(value);
+    // Scroll to current time indicator after timescale change
+    // to display it in view to let the user know where they are
+    queueMicrotask(() => this.scrollToCurrent());
+  }
+
+  trackByColumnKey(index: number, item: TimelineColumnRange) {
+    return item.key;
+  }
+  //#endregion
+
+  //#region Current time indicator handling
+  currentColumnIndex = computed<number>(() => {
+    const columns = this.columns();
+    const todayFormatted = new Date(this.today.getFullYear(), this.today.getMonth(), this.today.getDate());
+    return columns.findIndex(col => todayFormatted >= col.start && todayFormatted < col.end);
+  });
+
+  currentLineLeftPx = computed<number>(() => {
+    const index = this.currentColumnIndex();
+    if(index === -1) return 0;
+
+    const cols = this.columns();
+    const col = cols[index];
+    const totalWidth = this.columnWidthPx();
+
+    // this is a hack for now to adjust the line to start at the beginning of the month or week
+    // I believe a style is causing some offset that I have not yet identified
+    // for Day option it seems to work fine
+    if (this.timeScale() === 'month') {
+      return (index+1) * totalWidth;
+    }
+    if (this.timeScale() === 'week') {
+      return (index) * totalWidth;
+    }
+
+    const start = col.start.getTime();
+    const end = col.end.getTime();
+    const todayTime = new Date(this.today.getFullYear(), this.today.getMonth(), this.today.getDate()).getTime();
+
+    const fraction = (todayTime - start) / (end - start); // obtain fraction of the day within the column
+    return Math.round(index * totalWidth + fraction * totalWidth); // calculate left position based on index and fraction
+  });
+
+  currentLabel = computed<string>(() => {
+    const t = this.timeScale();
+    if(t === 'month')  return 'Current month';
+    if(t === 'week')   return 'Current week';
+    return 'Today'; // day
+  });
+
+  private scrollToCurrent() {
+    const elem = this.scrollElement.nativeElement;
+    if(!elem) return;
+    
+    // get current left position
+    const currentLeft = this.currentLineLeftPx();
+    // calculate target scroll position to center the current time indicator
+    // we offset by 35% of the element width to position it slightly left of center
+    // this provides better context of upcoming timeline. I played a little and 35% felt better than 50%
+    const target = Math.max(0, currentLeft - Math.round(elem.clientWidth * 0.35)); 
+    // smooth scroll to target position this way the user sees the movement to current time indicator
+    elem.scrollTo({ left: target, behavior: 'smooth' });
+  }
+  //#endregion
+
+  //#region Lifecycle hooks
+  ngAfterViewInit(): void {
+    // Initial scroll to current time indicator
+    queueMicrotask(() => this.scrollToCurrent());
+  }
+  //#endregion
 }
---
+++ work-order-schedule/src/app/utils/timeline-range.ts
@@ -0,0 +1,112 @@
+import { time } from "console";
+import { Timescale } from "../models/timeline.types";
+
+export type TimelineColumnRange = {
+    key: string; //id of the column
+    label: string; // display label of the column
+    start: Date; // start date of the column
+    end: Date; // end date of the column
+};
+
+const MONTH_FORMAT: Intl.DateTimeFormatOptions = { month: 'short' };
+const LOCAL_US = 'en-US';
+
+// Start date of the day (00:00:00)
+function startOfDay(date: Date): Date {
+    return new Date(date.getFullYear(), date.getMonth(), date.getDate());
+}
+
+// Add days to a date
+function addDays(date: Date, days: number): Date {
+    const result = new Date(date);
+    result.setDate(result.getDate() + days);
+    return result;
+}
+
+// Start date of the week (Monday as first day)
+function startOfWeekMonday(date: Date): Date {
+    const x = startOfDay(date);
+    const day = x.getDay();
+    const diff = (day === 0 ? -6 : 1) - day; // adjust when day is sunday
+    return addDays(x, diff);
+}
+
+// Start date of the month (1st day)
+function startOfMonth(date: Date): Date {
+    return new Date(date.getFullYear(), date.getMonth(), 1);
+}
+
+// Add months to a date
+function addMonths(date: Date, months: number): Date {
+    const result = new Date(date);
+    result.setMonth(result.getMonth() + months);
+    return result;
+}
+
+// Give format like 'Jan', 'Feb', etc.
+function formatMonth(date: Date): string {
+    return date.toLocaleString(LOCAL_US, MONTH_FORMAT) +` ${date.getFullYear().toString().slice(-2)}`;
+}
+
+function formatWeekLabel(start: Date): string {
+    const m = start.toLocaleDateString(LOCAL_US, MONTH_FORMAT);
+    const day = start.getDate().toString().padStart(2, '0');
+    return `Week ${m} ${day}`;
+}
+function formatDayLabel(date: Date): string {
+    const m = date.toLocaleDateString(LOCAL_US, MONTH_FORMAT);
+    const day = date.getDate().toString().padStart(2, '0');
+    return `${m} ${day}`;
+}
+
+export function buildColumnRanges(timescale: Timescale, anchor = new Date()): TimelineColumnRange[] {
+     const today = startOfDay(anchor);
+
+     if(timescale === 'month') {
+        const start = addMonths(startOfMonth(today), -2);
+        const count = 9; // 9 months total (2 back + current + 6 forward = 9)
+
+        return Array.from({ length: count }).map((_, i) => {
+            const s = addMonths(start, i);
+            const e = addMonths(s, i+1);
+            const label = formatMonth(s);
+            return {
+                key: `${s.getFullYear()}-${s.getMonth() + 1}`,
+                label,
+                start: s,
+                end: e,
+            };
+        });
+     }
+     else if(timescale === 'week') {
+        const start = addDays(startOfWeekMonday(today), -14); // 2 weeks back
+        const count = 13; // 13 weeks total (2 back + current + 10 forward = 13)
+        
+        return Array.from({ length: count }).map((_, i) => {
+            const s = addDays(start, i * 7);
+            const e = addDays(s, 7);
+            const label = formatWeekLabel(s);
+            return {
+                key: `${s.getFullYear()}-${s.getMonth() + 1}-${s.getDate()}`,
+                label,
+                start: s,
+                end: e,
+            };
+        });
+     }    else { // day
+        const start = addDays(today, -14); // 14 days back
+        const count = 57; // 57 days total (14 back + current + 42 forward = 57)
+
+        return Array.from({ length: count }).map((_, i) => {
+            const s = addDays(start, i);
+            const e = addDays(s, 1);
+            const label = formatDayLabel(s);
+            return {
+                key: `${s.getFullYear()}-${s.getMonth() + 1}-${s.getDate()}`,
+                label,
+                start: s,
+                end: e,
+            };
+        });
+    }
+}
